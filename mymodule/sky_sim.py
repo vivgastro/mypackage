02befa6b4001a79fc7fd9d7d69c88d18aa8f773f
Vivek Gupta <vivg269@gmail.com>
2023-03-22 12:00:51 +1100

    Shared mem multip version from Paul

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,10 +1,12 @@
 import math
 import numpy as np
 import multiprocessing
+from multiprocessing.shared_memory import SharedMemory
+import uuid
 import sys
 
 NSRC = 1_000_000
-
+mem_id = None
 
 def get_radec():
     # from wikipedia
@@ -23,65 +25,68 @@ def get_radec():
 def make_stars(args):
     """
     """
-    #unpack the arguments
-    ra, dec, nsrc = args
-    # create an empy array for our results
-    radec = np.empty((2,nsrc))
+    ra,dec,shape, nsrc, job_id = args    
+    # Find the shared memory and create a numpy array interface
+    shmem = SharedMemory(name=f'radec_{mem_id}', create=False)
+    radec = np.ndarray(shape, buffer=shmem.buf, dtype=np.float64)
 
     # make nsrc stars within 1 degree of ra/dec
-    radec[0,:] = np.random.uniform(ra-1, ra+1, size=nsrc)
-    radec[1,:] = np.random.uniform(dec-1, dec+1, size=nsrc)
-    
-    # return our results
-    return radec
+    ras = np.random.uniform(ra-1, ra+1, size=nsrc)
+    decs = np.random.uniform(dec-1, dec+1, size=nsrc)
 
-def make_stars_parallel(ra, dec, nsrc=NSRC, cores=None):
+    start = job_id * nsrc
+    end = start + nsrc
+    radec[0, start:end] = ras
+    radec[1, start:end] = decs
+    return
+
+def make_stars_parallel(ra,dec,nsrc=NSRC, cores=None):
     
     # By default use all available cores
     if cores is None:
         cores = multiprocessing.cpu_count()
     
-
     # 20 jobs each doing 1/20th of the sources
-    group_size = nsrc//20
-    args = [(ra, dec, group_size) for _ in range(20)]
+    args = [(ra, dec, (2,nsrc), nsrc//20, i) for i in range(20)]
 
-
-    # start a new process for each task, hopefully to reduce residual
-    # memory use
-    ctx = multiprocessing.get_context()
-    pool = ctx.Pool(processes=cores, maxtasksperchild=1)
-
+    exit = False
     try:
-        # call make_posisions(a) for each a in args
-        results = pool.map(make_stars, args, chunksize=1)
-    except KeyboardInterrupt:
-        # stop all the processes if the user calls the kbd interrupt
-        print("Caught kbd interrupt")
-        pool.close()
-        sys.exit(1)
-    else:
-        # join the pool means wait until there are results
-        pool.close()
-        pool.join()
+        # set up the shared memory
+        global mem_id
+        mem_id = str(uuid.uuid4())
 
-        # crete an empty array to hold our results
-        radec = np.empty((2,nsrc),dtype=np.float64)
+        nbytes = 2 * nsrc * np.float64(1).nbytes
+        radec = SharedMemory(name=f'radec_{mem_id}', create=True, size=nbytes)
 
-        # iterate over the results (a list of whatever was returned from make_stars)
-        for i,r in enumerate(results):
-            # store the returned results in the right place in our array
-            start = i*group_size
-            end = start + group_size
-            radec[:,start:end] = r
-            
-    return radec
+        # start a new process for each task, hopefully to reduce residual
+        # memory use
+        ctx = multiprocessing.get_context()
+        pool = ctx.Pool(processes=cores, maxtasksperchild=1)
+        try:
+            pool.map_async(make_stars, args, chunksize=1).get(timeout=10_000_000)
+        except KeyboardInterrupt:
+            print("Caught kbd interrupt")
+            pool.close()
+            exit = True
+        else:
+            pool.close()
+            pool.join()
+            # make sure to .copy() or the data will dissappear when you unlink the shared memory
+            local_radec = np.ndarray((2, nsrc), buffer=radec.buf,
+                                     dtype=np.float64).copy()
+    finally:
+        radec.close()
+        radec.unlink()
+        if exit:
+            sys.exit(1)
+    return local_radec
+
+
 
 if __name__ == "__main__":
     ra,dec = get_radec()
-    pos = make_stars_parallel(ra, dec, NSRC, 2)
+    pos = make_stars_parallel(ra,dec, NSRC, 2)
     # now write these to a csv file for use by my other program
     with open('catalog.csv', 'w') as f:
         print("id,ra,dec", file=f)
-        np.savetxt(f, np.column_stack((np.arange(NSRC), pos[0,:].T, pos[1,:].T)),fmt='%07d, %12f, > %12f')
-
+        np.savetxt(f, np.column_stack((np.arange(NSRC), pos[0,:].T, pos[1,:].T)),fmt='%07d, %12f, %12f')
